5b3fe5d6d8399a8611bf1d62bfe6bcc234296a79
Keval Pipalia <keval.pipalia009@yahoo.com>
2020-10-06 23:54:33 +0530

    tried flasgger

--- flasggerui.py
+++ flasggerui.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 30 18:27:47 2020
+Created on Tue Oct  6 21:35:01 2020
 
 @author: keval
 """
-
 
 import pandas as pd
 import numpy as np
 from flask import Flask,request
 import pickle
-
+import flasgger
+from flasgger import Swagger
 
 app = Flask(__name__)
+Swagger(app)
+
 pkl_in = open('classifier.pkl','rb')
 classifier = pickle.load(pkl_in)
 
@@ -22,6 +24,32 @@ def welcome():
 
 @app.route('/predict')
 def predict_note():
+    
+    """Fake Currency note detection System
+    ---
+    parameters:
+        - name: variance
+          in: query
+          type: number
+          required: true
+        - name: skewness
+          in: query
+          type: number
+          required: true
+        - name: curtosis
+          in: query
+          type: number
+          required: true
+        - name: entropy
+          in: query
+          type: number
+          required: true
+    responces:
+        200:
+            description: The prediction is
+        
+    """
+    
     variance = request.args.get('variance')
     skewness = request.args.get('skewness')
     curtosis = request.args.get('curtosis')
@@ -31,6 +59,18 @@ def predict_note():
 
 @app.route('/predict_file', methods=["POST"])
 def predict_note_file():
+    """Fake currency note detection System
+    ---
+    parameters:
+        - name: file
+          in: formData
+          type: file
+          required: true
+    responses:
+        200:
+             The predictions are
+    """
+        
     df_test = pd.read_csv(request.files.get("file"))
     pred = classifier.predict(df_test) 
     return "Your note measurements predicts it is" + str(list(pred))
